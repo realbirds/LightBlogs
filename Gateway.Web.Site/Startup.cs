d7cbb92979df8dede1b2fca0b3718bb45a48aafd
realbirds <realbird@hotmail.com>
2017-06-04 21:54:47 +0800

    ignore

--- Gateway.Web.Site/Startup.cs
+++ Gateway.Web.Site/Startup.cs
@@ -12,13 +12,12 @@ namespace Gateway.Web.Site
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
@@ -28,10 +27,12 @@ namespace Gateway.Web.Site
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
+
         }
     }
 }
