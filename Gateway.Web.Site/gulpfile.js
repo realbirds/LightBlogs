aeec234d92b0d3689685e3faf48df2f36d422e4b
realbirds <realbird@hotmail.com>
2017-06-05 23:10:17 +0800

    ignore

---
+++ Gateway.Web.Site/gulpfile.js
@@ -0,0 +1,27 @@
+﻿var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify');
+
+var $ = require('gulp-load-plugins')();
+
+gulp.task('es6', function () {
+
+    return gulp.src('wwwroot/src/js/**/*.t.js')
+        .pipe($.plumber())
+        .pipe($.babel({
+            presets: ['es2015']
+        }))
+        .pipe(rename(function(path) {
+            path.basename = path.basename.replace(".t", "");
+            path.extname = ".js";
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('wwwroot/dist/js/'));
+
+});
+
+gulp.task('watch', ['es6'], function () {
+    gulp.watch(['wwwroot/src/js/**/*.t.js'], ['es6']);
+});
